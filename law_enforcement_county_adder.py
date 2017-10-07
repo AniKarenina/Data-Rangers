55ada3241219c84f9258807eb985a9b189c0f264
wwetzel <wwetzel@users.noreply.github.com>
2017-10-07 18:59:39 -0500

    more functions

--- law_enforcement_county_adder.py
+++ law_enforcement_county_adder.py
@@ -1,29 +1,95 @@
 #######################
-# William Wetzel
-# MIT License
-# 8086 
+### William Wetzel  ###
+### MIT License     ###
+### 8086            ###
 #######################
-
-# The counties for the year 2007 in the law_enforcement employment data were entered by hand
-# To propagate counties to the rest of the data this script was used
-# Some hand cleaning was conducted where I had to standardize department naming conventions - doing it by hand was faster than writing a script
 
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('law_enforcement_2007-2017.csv')
+#####################################
+### law_enforcement_county_copier ###
+#####################################
 
-nRows = df.shape[0]
+# Used to propagate counties for each department throughout the dataset
+def law_enforcement_county_copier():
+    df = pd.read_csv('law_enforcement_2007-2017.csv')
 
-counties = {}
+    nRows = df.shape[0]
 
-for row in range(nRows):
-    if  pd.notnull(df.loc[row, 'county']):
-        counties[df.loc[row, 'department']] = df.loc[row, 'county']
+    counties = {}
+
+    for row in range(nRows):
+        if  pd.notnull(df.loc[row, 'county']):
+            counties[df.loc[row, 'department']] = df.loc[row, 'county']
+
+    for row in range(nRows):
+        if df.loc[row, 'department'] in counties:
+            df.loc[row, 'county'] = counties[df.loc[row, 'department']]
+
+    df.to_csv('law_enforcement_COUNTIES_2007-2017.csv', index=False)
+    
+######################
+### period_remover ###
+######################
+
+# Used to turn "P.D." to "PD"
+def period_remover(csv):
+    df = pd.read_csv(csv)
+
+    nRows = df.shape[0]
+
+    df['department'] = df['department'].str.replace('.', '')
+    df.to_csv('law_enforcement_2007-2017_pd.csv', index=False)
+    
+
+###########################
+### department_merger ###
+###########################
+
+# Used to find departments present in "law_enforcement_2007-2017.csv" but not "All_Crimes_by_Year_and_Agency.csv"
+# Used to merge department crime data by year with employment statistics by year
+def department_merger(law, crime):
+    
+    dfLaw = pd.read_csv(law)
+    dfCrime = pd.read_table(crime, skiprows=[1,2])
+    
+    # Making a few missing department names match
+    dfCrime['Agency'] = dfCrime['Agency'].str.replace('.', '')
+    dfCrime['Agency'] = dfCrime['Agency'].str.replace('South Sioux City PD', 'So Sioux City PD')
+    dfCrime['Agency'] = dfCrime['Agency'].str.replace('La Vista PD', 'LaVista PD')
+
+    # Checking if all departments between the 2 csvs match
+    departmentResults = dfLaw['department'].isin(dfCrime['Agency'])
+    
+    unmatchedDepartments = []
+    for row in range(departmentResults.size):
+        if departmentResults[row] == False:
+            unmatchedDepartments.append(dfLaw.loc[row, 'department'])    
+    # Get unique names of departments which don't match        
+    unmatchedDepartments = set(unmatchedDepartments)
+        
+    # Merging the 2 dataframes on department and year
+    lawCrimeMerged = dfLaw
+    years = ['2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016']
+    
+    # Initialize a new column for total crime
+    lawCrimeMerged['total_crime'] = np.nan
+    # For each 
+    for row in range(lawCrimeMerged.shape[0]):
+        tempDataFrame = (dfCrime.loc[dfCrime['Agency'] == lawCrimeMerged.loc[row, 'department']])
+        for year in years:
+            number = int(tempDataFrame[year])
+            index = lawCrimeMerged.index[(lawCrimeMerged['department'] == lawCrimeMerged.loc[row, 'department']) & (lawCrimeMerged['year'] == int(year))].tolist()
+            lawCrimeMerged.loc[index, 'total_crime'] = number            
+
+    lawCrimeMerged.to_csv('lawCrimeMerged.csv', index=False)
 
 
-for row in range(nRows):
-    if df.loc[row, 'department'] in counties:
-        df.loc[row, 'county'] = counties[df.loc[row, 'department']]
+# Run individual functions here
 
-df.to_csv('law_enforcement_COUNTIES_2007-2017.csv', index=False)
+#law_enforcement_county_copier():   
+#period_remover('law_enforcement_2007-2017.csv')
+#department_merger('law_enforcement_2007-2017.csv', 'All_Crimes_by_Year_and_Agency.txt')
+
+
